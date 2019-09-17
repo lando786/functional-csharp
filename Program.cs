d8a7def2b9968f92209ee86e5aeb8d3018b0c600
Luis Dasta <ldasta786@gmail.com>
2019-09-17 17:32:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace functional_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
